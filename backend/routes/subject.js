e9da487969a61328f5ff0de8f039178baed01149
Nong <naong69@gmail.com>
2025-05-09 16:40:02 +0700

    assignment#1

---
+++ backend/routes/subject.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const subject = require('../data/contact_subject.json');
+
+router.get('/', (req, res) => {
+    //res.end('{"contactSubject": ["General Enquiry","Class","Schedule","Instructor","Price","Location","Other","nong","noppachai"]}');
+    res.json(subject);
+});
+
+module.exports = router;
+
+
